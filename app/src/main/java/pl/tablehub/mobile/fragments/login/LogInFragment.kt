0a7e8ebf6a90bfb969ed93da42d669f1f480ee50
Julian Bednarek <62265719+julian-bednarek@users.noreply.github.com>
2025-04-20 08:36:52 +0200

    nowy .gitignore (#7)
    * naprawione po czosnku
    
    * updated gitingore

--- app/src/main/java/pl/tablehub/mobile/fragments/login/LogInFragment.kt
+++ app/src/main/java/pl/tablehub/mobile/fragments/login/LogInFragment.kt
@@ -4,14 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.compose.material3.Text
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.ViewCompositionStrategy
 import androidx.fragment.app.Fragment
 import dagger.hilt.android.AndroidEntryPoint
 import pl.tablehub.mobile.fragments.login.composables.MainLoginView
-import pl.tablehub.mobile.ui.theme.TableHubTheme
 
 @AndroidEntryPoint
 class LogInFragment : Fragment() {
--- app/src/main/java/pl/tablehub/mobile/fragments/login/composables/CredentialsInputs.kt
+++ app/src/main/java/pl/tablehub/mobile/fragments/login/composables/CredentialsInputs.kt
@@ -22,7 +22,6 @@ import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
-
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
