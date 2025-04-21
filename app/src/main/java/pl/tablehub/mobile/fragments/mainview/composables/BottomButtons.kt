a88c285f61848da5c2d71acc741f084a2e2e4927
Julian Bednarek <juliuszxxbednarek@gmail.com>
2025-04-21 10:08:01 +0200

    make scaffolding for the main view (menu to be added + buttons functions) v2 (sth went wrong with previous commit)

---
+++ app/src/main/java/pl/tablehub/mobile/fragments/mainview/composables/BottomButtons.kt
@@ -0,0 +1,59 @@
+package pl.tablehub.mobile.fragments.mainview.composables
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.LocationOn
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import pl.tablehub.mobile.R
+import pl.tablehub.mobile.ui.shared.composables.constants.BUTTON_HEIGHT
+import pl.tablehub.mobile.ui.shared.composables.constants.CORNER_ROUND_SIZE
+import pl.tablehub.mobile.ui.shared.composables.constants.HORIZONTAL_PADDING
+import pl.tablehub.mobile.ui.shared.composables.constants.ICON_SIZE
+import pl.tablehub.mobile.ui.shared.composables.constants.VERTICAL_PADDING
+import pl.tablehub.mobile.ui.theme.PRIMARY_COLOR
+import pl.tablehub.mobile.ui.theme.SECONDARY_COLOR
+
+@Composable
+fun BottomButtons(
+    onReportClick: () -> Unit,
+    onLocationClick: () -> Unit
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = HORIZONTAL_PADDING.dp, vertical = VERTICAL_PADDING.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        MainViewButton(
+            text = stringResource(R.string.report_free_tables),
+            onClick = onReportClick
+        )
+        IconButton(onClick = onLocationClick,
+            modifier = Modifier
+                .size(BUTTON_HEIGHT.dp)
+                .background(
+                    color = SECONDARY_COLOR,
+                    shape = RoundedCornerShape(CORNER_ROUND_SIZE.dp))
+        ) {
+            Icon(
+                imageVector = Icons.Default.LocationOn,
+                contentDescription = stringResource(R.string.current_location),
+                tint = PRIMARY_COLOR,
+                modifier = Modifier.size(ICON_SIZE.dp)
+            )
+        }
+    }
+} 
