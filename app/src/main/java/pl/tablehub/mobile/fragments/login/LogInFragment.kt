0f312587e380177b3a9336c36a80bf58ef21e291
BartekS5 <seczkowskibartek@gmail.com>
2025-04-22 13:21:48 +0200

    pulled main

--- app/src/main/java/pl/tablehub/mobile/fragments/login/LogInFragment.kt
+++ app/src/main/java/pl/tablehub/mobile/fragments/login/LogInFragment.kt
@@ -9,13 +9,9 @@ import androidx.compose.ui.platform.ViewCompositionStrategy
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import dagger.hilt.android.AndroidEntryPoint
-<<<<<<< HEAD
 import pl.tablehub.mobile.R
 import pl.tablehub.mobile.fragments.login.composables.MainLoginView
-=======
-import pl.tablehub.mobile.fragments.login.composables.MainLoginView
-import pl.tablehub.mobile.ui.theme.TableHubTheme
->>>>>>> 6b09a05c76ebb26650abc485a850eff51d5be7ac
+
 
 @AndroidEntryPoint
 class LogInFragment : Fragment() {
@@ -27,15 +23,11 @@ class LogInFragment : Fragment() {
         return ComposeView(requireContext()).apply {
             setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
             setContent {
-<<<<<<< HEAD
                 MainLoginView(
                     onRegister = {
                         findNavController().navigate(R.id.action_logInFragment_to_mainViewFragment)
                     }
                 )
-=======
-                MainLoginView()
->>>>>>> 6b09a05c76ebb26650abc485a850eff51d5be7ac
             }
         }
     }
