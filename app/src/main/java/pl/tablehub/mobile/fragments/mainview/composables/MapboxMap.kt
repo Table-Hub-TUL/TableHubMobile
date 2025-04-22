f8f9baf8c8a551c9a1721c40acc6a348c6b151cc
Julian Bednarek <juliuszxxbednarek@gmail.com>
2025-04-22 21:45:36 +0200

    location (i.e. your current location works)

--- app/src/main/java/pl/tablehub/mobile/fragments/mainview/composables/MapboxMap.kt
+++ app/src/main/java/pl/tablehub/mobile/fragments/mainview/composables/MapboxMap.kt
@@ -3,30 +3,53 @@ package pl.tablehub.mobile.fragments.mainview.composables
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.mapbox.geojson.Point
+import com.mapbox.maps.extension.compose.MapEffect
 import com.mapbox.maps.extension.compose.MapboxMap
 import com.mapbox.maps.extension.compose.animation.viewport.rememberMapViewportState
+import com.mapbox.maps.plugin.PuckBearing
+import com.mapbox.maps.plugin.locationcomponent.createDefault2DPuck
+import com.mapbox.maps.plugin.locationcomponent.location
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.collectLatest
 
 @Composable
-fun MapboxMapWrapper() {
+fun MapboxMapWrapper(locationTrigger: SharedFlow<Unit>) {
+    val mapViewportState = rememberMapViewportState {
+        setCameraOptions {
+            zoom(18.0)
+            center(Point.fromLngLat(19.578037, 51.78987))
+            pitch(0.0)
+            bearing(0.0)
+        }
+    }
     MapboxMap(
         Modifier.fillMaxSize(),
-        mapViewportState = rememberMapViewportState {
-            setCameraOptions {
-                zoom(18.0)
-                center(Point.fromLngLat(19.578037, 51.78987))
-                pitch(0.0)
-                bearing(0.0)
-            }
-        },
+        mapViewportState = mapViewportState,
         compass = {
             Compass(
                 alignment = Alignment.BottomEnd,
                 modifier = Modifier.padding(start = 0.dp, bottom = 100.dp, end = 8.dp)
             )
         }
-    )
+    ) {
+        MapEffect(Unit) { mapView ->
+            mapView.location.updateSettings {
+                locationPuck = createDefault2DPuck(withBearing = true)
+                enabled = true
+                puckBearing = PuckBearing.COURSE
+                puckBearingEnabled = true
+            }
+        }
+    }
+
+    LaunchedEffect(locationTrigger, mapViewportState) {
+        locationTrigger.collectLatest {
+            mapViewportState.transitionToFollowPuckState()
+        }
+    }
 }
