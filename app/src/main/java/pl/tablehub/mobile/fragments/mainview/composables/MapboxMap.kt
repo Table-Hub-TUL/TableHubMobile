a1bf79a232c3f2dbd029a3da1323b52c87c5829a
Julian Bednarek <juliuszxxbednarek@gmail.com>
2025-04-22 14:13:29 +0200

    compass works

--- app/src/main/java/pl/tablehub/mobile/fragments/mainview/composables/MapboxMap.kt
+++ app/src/main/java/pl/tablehub/mobile/fragments/mainview/composables/MapboxMap.kt
@@ -1,8 +1,11 @@
 package pl.tablehub.mobile.fragments.mainview.composables
 
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.mapbox.geojson.Point
 import com.mapbox.maps.extension.compose.MapboxMap
 import com.mapbox.maps.extension.compose.animation.viewport.rememberMapViewportState
@@ -13,11 +16,17 @@ fun MapboxMapWrapper() {
         Modifier.fillMaxSize(),
         mapViewportState = rememberMapViewportState {
             setCameraOptions {
-                zoom(2.0)
-                center(Point.fromLngLat(21.0, 52.0))
+                zoom(18.0)
+                center(Point.fromLngLat(19.578037, 51.78987))
                 pitch(0.0)
                 bearing(0.0)
             }
         },
+        compass = {
+            Compass(
+                alignment = Alignment.BottomEnd,
+                modifier = Modifier.padding(start = 0.dp, bottom = 100.dp, end = 8.dp)
+            )
+        }
     )
 }
