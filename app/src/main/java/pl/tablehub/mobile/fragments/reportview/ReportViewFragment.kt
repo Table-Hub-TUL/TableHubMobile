721a065024fc580afa98e3807e57d48ad7257671
Julian Bednarek <62265719+julian-bednarek@users.noreply.github.com>
2025-04-23 08:47:15 +0200

    empty classes for czosnek (#15)
    * test commit
    
    * centered "report free tables" button
    
    * location (i.e. your current location works)
    
    * empty classes for czosnek

---
+++ app/src/main/java/pl/tablehub/mobile/fragments/reportview/ReportViewFragment.kt
@@ -0,0 +1,8 @@
+package pl.tablehub.mobile.fragments.reportview
+
+import androidx.fragment.app.Fragment
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class ReportViewFragment : Fragment() {
+}
---
+++ app/src/main/java/pl/tablehub/mobile/fragments/settings/SettingsFragment.kt
@@ -0,0 +1,8 @@
+package pl.tablehub.mobile.fragments.settings
+
+import androidx.fragment.app.Fragment
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class SettingsFragment : Fragment() {
+}
