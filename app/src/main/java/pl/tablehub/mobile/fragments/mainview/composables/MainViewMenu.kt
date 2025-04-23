06b47ceaae722267123f05d30b1a9c2c2b864e6c
Julian Bednarek <62265719+julian-bednarek@users.noreply.github.com>
2025-04-23 15:18:25 +0200

    menu (looking not working) (#17)
    * loginscreen
    
    * rebasing
    
    * loginscreen
    
    * rebasing
    
    * loginscreen
    
    * pulled main
    
    * menureturn added
    
    * menu additions
    
    ---------
    
    Co-authored-by: BartekS5 <seczkowskibartek@gmail.com>

--- app/src/main/java/pl/tablehub/mobile/fragments/mainview/composables/MainViewMenu.kt
+++ app/src/main/java/pl/tablehub/mobile/fragments/mainview/composables/MainViewMenu.kt
@@ -1,29 +1,68 @@
 package pl.tablehub.mobile.fragments.mainview.composables
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.DrawerState
 import androidx.compose.material3.ModalDrawerSheet
 import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import kotlinx.coroutines.launch
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.DrawerDefaults
+import pl.tablehub.mobile.R
+import pl.tablehub.mobile.ui.theme.SECONDARY_COLOR
 
 @Composable
 fun MainViewMenu(
     drawerState: DrawerState,
-    content: @Composable () -> Unit = {},
+    onProfileClick: () -> Unit = {},
+    onHistoryClick: () -> Unit = {},
+    onSettingsClick: () -> Unit = {},
+    content: @Composable () -> Unit = {}
 ) {
     val scope = rememberCoroutineScope()
     ModalNavigationDrawer(
+        modifier = Modifier.background(SECONDARY_COLOR),
         drawerState = drawerState,
         gesturesEnabled = false,
         drawerContent = {
             ModalDrawerSheet {
-                // Drawer content goes here - currently empty as requested
-                MainViewMenuReturnButton(onReturnClick={
-                    scope.launch{
-                        drawerState.close()
+                Box(modifier = Modifier
+                    .fillMaxSize()
+                    .background(SECONDARY_COLOR)) {
+                    Column {
+                        MainViewMenuReturnButton(onReturnClick = {
+                            scope.launch {
+                                drawerState.close()
+                            }
+                        })
+                        Spacer(modifier = Modifier.height(8.dp))
+                        MainViewMenuButton(
+                            onClick = onProfileClick,
+                            imgName = R.drawable.profile,
+                            stringFromRes = R.string.profile
+                        )
+                        Spacer(modifier = Modifier.height(8.dp))
+                        MainViewMenuButton(
+                            onClick = onHistoryClick,
+                            imgName = R.drawable.history,
+                            stringFromRes = R.string.history
+                        )
+                        Spacer(modifier = Modifier.height(8.dp))
+                        MainViewMenuButton(
+                            onClick = onSettingsClick,
+                            imgName = R.drawable.settings,
+                            stringFromRes = R.string.settings
+                        )
                     }
-                })
+                }
             }
         }
     ) {
