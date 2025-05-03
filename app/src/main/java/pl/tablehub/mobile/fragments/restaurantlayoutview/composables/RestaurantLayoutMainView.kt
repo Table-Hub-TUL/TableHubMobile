b8e3342f0b10a99506343fefecd34be17644a650
Julian Bednarek <62265719+julian-bednarek@users.noreply.github.com>
2025-05-03 17:42:48 +0200

    Model improvements and layoutview (#24)
    * coplito fixes + parcelable impl
    
    * troche tu sie nazbieralo
    
    * added empty restaurant layout view

---
+++ app/src/main/java/pl/tablehub/mobile/fragments/restaurantlayoutview/composables/RestaurantLayoutMainView.kt
@@ -0,0 +1,11 @@
+package pl.tablehub.mobile.fragments.restaurantlayoutview.composables
+
+import androidx.compose.runtime.Composable
+import pl.tablehub.mobile.model.Restaurant
+
+@Composable
+fun RestaurantLayoutMainView(
+    restaurant: Restaurant
+) {
+
+}
