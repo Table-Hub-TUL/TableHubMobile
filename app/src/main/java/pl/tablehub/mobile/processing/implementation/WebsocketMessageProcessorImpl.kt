207d33e10914d3265ec4c59ad3f629f4130866a8
Julian Bednarek <juliuszxxbednarek@gmail.com>
2025-05-18 09:04:49 +0200

    simple message processor

--- app/src/main/java/pl/tablehub/mobile/processing/implementation/WebsocketMessageProcessorImpl.kt
+++ app/src/main/java/pl/tablehub/mobile/processing/implementation/WebsocketMessageProcessorImpl.kt
@@ -7,6 +7,10 @@ import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.cancel
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
+import pl.tablehub.mobile.model.websocket.MessageType
+import pl.tablehub.mobile.model.websocket.RestaurantSubscriptionResponse
+import pl.tablehub.mobile.model.websocket.RestaurantsResponse
+import pl.tablehub.mobile.model.websocket.TableStatusChange
 import pl.tablehub.mobile.model.websocket.WebSocketMessage
 import pl.tablehub.mobile.processing.interfaces.IWebsocketMessageProcessor
 import pl.tablehub.mobile.repository.IRestaurantsRepository
@@ -36,7 +40,30 @@ class WebsocketMessageProcessorImpl @Inject constructor(
     }
 
     override suspend fun process(webSocketMessage: WebSocketMessage) {
-        TODO("Not yet implemented")
+        when (webSocketMessage.header.type) {
+            MessageType.QUERY_RESTAURANTS_RESPONSE -> processRestaurantsResponse(webSocketMessage)
+            MessageType.SUBSCRIBE_RESTAURANT_UPDATES_RESPONSE -> processSubscriptionResponse(webSocketMessage)
+            MessageType.TABLE_STATUS_CHANGED_EVENT -> processTableStatusChangedEvent(webSocketMessage)
+            MessageType.UPDATE_TABLE_STATUS_RESPONSE -> {}
+            MessageType.ERROR_RESPONSE -> {}
+            else -> {}
+        }
+    }
+
+    private suspend fun processRestaurantsResponse(webSocketMessage: WebSocketMessage) {
+        val restaurantsResponse = webSocketMessage.body as RestaurantsResponse
+        restaurantsRepository.processRestaurantList(restaurantsResponse.restaurants)
+    }
+
+    private suspend fun processSubscriptionResponse(webSocketMessage: WebSocketMessage) {
+        (webSocketMessage.body as? RestaurantSubscriptionResponse)?.takeIf { it.success }?.let { response ->
+            restaurantsRepository.processInitialRestaurantData(response.initialState.id, response.initialState)
+        }
+    }
+
+    private suspend fun processTableStatusChangedEvent(webSocketMessage: WebSocketMessage) {
+        val tableStatusChangedEvent = webSocketMessage.body as TableStatusChange
+        restaurantsRepository.processTableStatusChange(tableStatusChangedEvent)
     }
 
     override fun stop() {
