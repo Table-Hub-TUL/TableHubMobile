48003a53d0507d482954fe240195f7982a7c1c48
BartekS5 <seczkowskibartek@gmail.com>
2025-06-19 20:53:16 +0200

    ╭∩╮-_-╭∩╮

--- app/src/main/java/pl/tablehub/mobile/fragments/mainview/composables/RestaurantDetailsPopUp.kt
+++ app/src/main/java/pl/tablehub/mobile/fragments/mainview/composables/RestaurantDetailsPopUp.kt
@@ -1,7 +1,5 @@
 package pl.tablehub.mobile.fragments.mainview.composables
 
-import android.content.Intent
-import android.net.Uri
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
@@ -16,7 +14,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
