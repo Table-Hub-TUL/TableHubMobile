cb08c677d35fdcd31c07cc8c8f2c2f0fde6ee918
BartekS5 <164517726+BartekS5@users.noreply.github.com>
2025-10-15 10:41:43 +0200

    color change

--- app/src/main/java/pl/tablehub/mobile/ui/theme/Color.kt
+++ app/src/main/java/pl/tablehub/mobile/ui/theme/Color.kt
@@ -2,10 +2,15 @@ package pl.tablehub.mobile.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val PRIMARY_COLOR: Color = Color(0xFFFF6A1A)
+val PRIMARY_COLOR: Color = Color(0xFFEF6A1A)
 val SECONDARY_COLOR: Color = Color(0xFFFFFFEB)
 val TERTIARY_COLOR: Color = Color(0xFF174A63)
 val GREEN_FREE_COLOR: Color = Color(0xFF8CE563)
+val RED_OCCUPIED_COLOR: Color = Color(0xFFE56363)
+val GRAY_UNKNOWN_COLOR: Color = Color(0xFF999999)
+val GRAY_TABLE_TEXT_COLOR: Color = Color(0xFF2B2B2B)
+val WHITE_COLOR: Color = Color(0xFFFFFFFF)
+
 
 val DARK_PRIMARY_COLOR: Color = Color(0xFFFFAB40)
 val DARK_SECONDARY_COLOR: Color = Color(0xFF333333)
