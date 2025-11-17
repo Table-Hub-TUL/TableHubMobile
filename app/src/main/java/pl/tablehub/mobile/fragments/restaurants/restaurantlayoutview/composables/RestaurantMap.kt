a1f97a7cb506f9b18ffbc6c9b0edfc0b7f163d15
Diana <dianaprosniewska@gmail.com>
2025-11-17 17:56:19 +0100

    zamykanie popup status stolika

--- app/src/main/java/pl/tablehub/mobile/fragments/restaurants/restaurantlayoutview/composables/RestaurantMap.kt
+++ app/src/main/java/pl/tablehub/mobile/fragments/restaurants/restaurantlayoutview/composables/RestaurantMap.kt
@@ -92,6 +92,7 @@ fun RestaurantMapRenderer(
                                 newStatus
                             )
                         )
+                        selectedTableDetail = null;
                     }
                 )
             }
